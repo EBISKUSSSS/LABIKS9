858adcd9659fced43af4e79d3f28a7065e7ac864
EBISKUSSSS <vladyk19.19@gmail.com>
2024-11-08 18:22:41 +0200

    laba9

---
+++ Controllers/lab9Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LABIKS9.Controllers
+{
+    public class lab9Controller : Controller
+    {
+        // GET: lab9
+        public ActionResult FirstViewMethod()
+        {
+            List<string> animals = GetAnimalsList();
+            return View(animals);
+        }
+
+        public ActionResult SecondViewMethod()
+        {
+            return View(GetAnimalsList().OrderBy(x => x).ToList());
+        }
+
+       public ActionResult ThirdViewMethod()
+        {
+            return View(GetAnimalsList().OrderBy(x => x).ToList());
+        }
+        public List<string> GetAnimalsList()
+        {
+            List<string> animals = new List<string>();
+            animals.Add("Doter");
+            animals.Add("Donkey Kong");
+            animals.Add("Petuh");
+            animals.Add("KFC Chicken strips");
+            animals.Add("Pig");
+            animals.Add("Kaban");
+            animals.Add("Zayats");
+            animals.Add("Grinch");
+            animals.Add("Crazy Rooster");
+            animals.Add("Crackhead");
+            animals.Add("Myravei");
+
+            return animals;
+        }
+    }
+}
